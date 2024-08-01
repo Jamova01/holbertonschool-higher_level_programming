45ade74afa2ff8b3a8463c179b73ecc8758d4208
Jamova01 <jorgemova01@gmail.com>
2024-08-01 00:08:10 -0500

    List all privileges of the MySQL users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
--- List all privileges of the MySQL users
--- Users: user_0d_1 and user_0d_2 on your server (in localhost).
+-- list all privileges of the MySQL users
+-- user_0d_1 and user_0d_2 on your server (in localhost).
+-- run: cat 0-privileges.sql | mysql -hlocalhost -uroot -p
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
