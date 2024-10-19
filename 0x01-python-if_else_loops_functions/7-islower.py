44369ebe42e850c9f9199fda7820d853f422dc6a
Jamova01 <jorgemova01@gmail.com>
2024-10-19 08:51:52 -0500

    Python function that checks for lowercase character

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,8 @@
 
 
 def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True
-    else:
-        return False
+    for i in range(97, 123):
+        if c == chr(i):
+            return True
+        else:
+            return False
